4c28ee9f4f29ebda5a391d67830dd6771d19f8d8
sagar-metastay <sagar.mahapatra@metastay.com>
2022-07-27 17:25:38 +0530

    just saving

--- src-def/form/FormComponentDef.ts
+++ src-def/form/FormComponentDef.ts
@@ -28,46 +28,32 @@ export class StepperMetaData {
   }
 }
 
-export class StepMetaData implements ComponentMetaDataProvider {
-  componentName = "v-step";
+export class StepMetaData  {
   id: string;
   name: string;
-  formList: FormMetaData[] = [];
-  componentList: ComponentMetaDataProvider[] = [];
+  component: ComponentMetaDataProvider;
 
   constructor({
     id,
     name,
-  }: // formList,
-  {
+    component,
+  }: {
     id: string;
     name: string;
-    // formList: Form[];
+    component: ComponentMetaDataProvider;
   }) {
     this.id = id;
     this.name = name;
-    // this.formList = formList;
+    this.component = component;
   }
 
-  addForm(form: FormMetaData) {
-    this.formList.push(form);
-  }
-
-  addComponent(comp: ComponentMetaDataProvider) {
-    this.componentList.push(comp);
-  }
-
+ 
   componentMetaData() {
     return {
       props: {
         id: this.id,
         name: this.name,
-        formList: this.formList.map((form: FormMetaData) =>
-          form.componentMetaData()
-        ),
-        componentList: this.componentList.map(
-          (comp: ComponentMetaDataProvider) => comp.componentMetaData()
-        ),
+        component: this.component,
       },
     };
   }
@@ -513,248 +499,6 @@ export class AddressMetaData implements ComponentMetaDataProvider {
         outlined: this.parentDataProvider.outlined,
         dense: this.parentDataProvider.dense,
         onChange: this.onChange,
-      },
-    };
-  }
-}
-
-export class CreditorMetaData implements ComponentMetaDataProvider {
-  // FIXED
-  componentName = "f-creditor";
-  // MANDATORY
-  id: string;
-  dataSelectorKey: string;
-  label: string;
-  // OPTIONAL with Default
-  rules: string;
-  colWidth: number;
-  mandatory: boolean;
-  disabled: boolean;
-  onChange: () => void;
-  parentDataProvider: FormChildMetaDataProvider;
-
-  constructor({
-    parentDataProvider,
-    id,
-    dataSelectorKey,
-    label,
-    rules = "",
-    colWidth = 12,
-    mandatory = false,
-    disabled = false,
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    onChange = () => {},
-  }: {
-    parentDataProvider: FormChildMetaDataProvider;
-    id?: string;
-    dataSelectorKey: string;
-    label: string;
-    rules?: string;
-    colWidth?: number;
-    mandatory?: boolean;
-    disabled?: boolean;
-    onChange?: () => void;
-  }) {
-    this.parentDataProvider = parentDataProvider;
-    this.id = !!id ? id : dataSelectorKey;
-    this.dataSelectorKey = dataSelectorKey;
-    this.label = label;
-    this.rules = rules;
-    this.colWidth = colWidth;
-    this.mandatory = mandatory;
-    this.disabled = disabled;
-    this.onChange = onChange;
-  }
-
-  getBoundaryClass() {
-    return `col-${this.colWidth} ${this.parentDataProvider.padding}`;
-  }
-
-  getRules() {
-    const mandarotyStr = this.mandatory ? "required" : "";
-    return `${mandarotyStr}|${this.rules}`;
-  }
-  componentMetaData(): object {
-    return {
-      componentName: this.componentName,
-      rules: this.getRules(),
-      boundaryClass: this.getBoundaryClass(),
-      props: {
-        key: this.dataSelectorKey,
-        name: this.dataSelectorKey, // todo: check the name functionalities
-        label: this.label,
-        disabled: this.disabled,
-        outlined: this.parentDataProvider.outlined,
-        dense: this.parentDataProvider.dense,
-        onChange: this.onChange,
-      },
-    };
-  }
-}
-
-export class BudgetMetaData implements ComponentMetaDataProvider {
-  componentName = "f-budget";
-
-  id: string;
-  dataSelectorKey?: string;
-  formList: FormMetaData[] = [];
-  actionList: ButtonMetaData[] = [];
-  parentDataProvider: ComponentMetaDataProvider;
-
-  constructor({
-    id,
-    dataSelectorKey,
-    parentDataProvider,
-  }: {
-    id: string;
-    dataSelectorKey?: string;
-    parentDataProvider: ComponentMetaDataProvider;
-  }) {
-    this.id = id;
-    this.parentDataProvider = parentDataProvider;
-    this.dataSelectorKey = dataSelectorKey;
-  }
-
-  addForm(childForm: FormMetaData) {
-    this.formList.push(childForm);
-    return this;
-  }
-
-  addAction(child: ButtonMetaData) {
-    this.actionList.push(child);
-    return this;
-  }
-
-  componentMetaData(): object {
-    return {
-      componentName: this.componentName,
-      parentDataProvider: this.parentDataProvider,
-      id: this.id,
-      dataSelectorKey: this.dataSelectorKey,
-      props: {
-        id: this.id,
-        formComponentMetaDataList: this.formList.map((form) => form.componentMetaData()),
-        actionList: this.actionList.map((action) => action.componentMetaData()),
-      },
-    };
-  }
-}
-
-export class DocumentMetaData implements ComponentMetaDataProvider {
-  // FIXED
-  componentName = "f-document";
-  // MANDATORY
-  id: string;
-  dataSelectorKey: string;
-  label: string;
-  // OPTIONAL with Default
-  rules: string;
-  colWidth: number;
-  mandatory: boolean;
-  disabled: boolean;
-  onChange: () => void;
-  parentDataProvider: FormChildMetaDataProvider;
-
-  constructor({
-    parentDataProvider,
-    id,
-    dataSelectorKey,
-    label,
-    rules = "",
-    colWidth = 12,
-    mandatory = false,
-    disabled = false,
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    onChange = () => {},
-  }: {
-    parentDataProvider: FormChildMetaDataProvider;
-    id?: string;
-    dataSelectorKey: string;
-    label: string;
-    rules?: string;
-    colWidth?: number;
-    mandatory?: boolean;
-    disabled?: boolean;
-    onChange?: () => void;
-  }) {
-    this.parentDataProvider = parentDataProvider;
-    this.id = !!id ? id : dataSelectorKey;
-    this.dataSelectorKey = dataSelectorKey;
-    this.label = label;
-    this.rules = rules;
-    this.colWidth = colWidth;
-    this.mandatory = mandatory;
-    this.disabled = disabled;
-    this.onChange = onChange;
-  }
-
-  getBoundaryClass() {
-    return `col-${this.colWidth} ${this.parentDataProvider.padding}`;
-  }
-
-  getRules() {
-    const mandarotyStr = this.mandatory ? "required" : "";
-    return `${mandarotyStr}|${this.rules}`;
-  }
-  componentMetaData(): object {
-    return {
-      componentName: this.componentName,
-      rules: this.getRules(),
-      boundaryClass: this.getBoundaryClass(),
-      props: {
-        key: this.dataSelectorKey,
-        name: this.dataSelectorKey, // todo: check the name functionalities
-        label: this.label,
-        disabled: this.disabled,
-        outlined: this.parentDataProvider.outlined,
-        dense: this.parentDataProvider.dense,
-        onChange: this.onChange,
-      },
-    };
-  }
-}
-
-export class PaymentPlanMetaData implements ComponentMetaDataProvider {
-  componentName = "f-payment-plan";
-
-  id: string;
-  dataSelectorKey?: string;
-  actionList: ButtonMetaData[] = [];
-  form: FormMetaData;
-  parentDataProvider: ComponentMetaDataProvider;
-
-  constructor({
-    id,
-    dataSelectorKey,
-    parentDataProvider,
-    form,
-  }: {
-    id: string;
-    dataSelectorKey?: string;
-    parentDataProvider: ComponentMetaDataProvider;
-    form: FormMetaData;
-  }) {
-    this.id = id;
-    this.parentDataProvider = parentDataProvider;
-    this.dataSelectorKey = dataSelectorKey;
-    this.form = form;
-  }
-
-  addAction(child: ButtonMetaData) {
-    this.actionList.push(child);
-    return this;
-  }
-
-  componentMetaData(): object {
-    return {
-      componentName: this.componentName,
-      parentDataProvider: this.parentDataProvider,
-      id: this.id,
-      dataSelectorKey: this.dataSelectorKey,
-      props: {
-        id: this.id,
-        formComponentMetaData: this.form.componentMetaData(),
-        actionList: this.actionList.map((action) => action.componentMetaData()),
       },
     };
   }

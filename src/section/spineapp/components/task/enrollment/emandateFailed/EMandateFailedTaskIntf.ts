44901796c29712a4f134a26a4ea752841708cc24
umesh-freed <umesh.yadav1@freed.care>
2022-08-24 11:11:16 +0530

    for renaing with UpperCase deleting

--- src/section/spineapp/components/task/enrollment/emandateFailed/EMandateFailedTaskIntf.ts
+++
@@ -1,7 +0,0 @@
-export default interface EMandateFailedTaskIntf {
-    $refs: any
-    taskDisabled: boolean
-    saveAndMarkCompleteTask: () => void
-    saveTask: () => void
-    gotoFile: () => void
-}

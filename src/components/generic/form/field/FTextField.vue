37538b141e0b5221e12aef68cfa909819f58bf49
sagar-freed <sagar.mahapatra@freed.care>
2022-08-29 10:38:02 +0530

    TextField condition

--- src/components/generic/form/field/FTextField.vue
+++ src/components/generic/form/field/FTextField.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-text-field v-if="condition" v-bind="$props" :value="modelValue"  @input="(newValue) => modelValue = newValue"></v-text-field>
+  <v-text-field v-bind="$props" :value="modelValue"  @input="(newValue) => modelValue = newValue"></v-text-field>
 </template>
 <script lang="ts">
 import { Component, Prop } from "vue-property-decorator";
@@ -11,9 +11,7 @@ import { VTextField } from "vuetify/lib/components";
   },
 })
 export default class FTextField extends VTextField {
-  @Prop({default: true})
-  condition: boolean;
-  
+
   // MODEL VALUE - START
   @Prop()
   value: string;

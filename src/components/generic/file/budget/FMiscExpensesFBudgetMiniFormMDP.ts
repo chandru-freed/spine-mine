0d0b508758dc00888b8054f476628428188ad043
govind-freed <govind.ve@freed.care>
2022-09-01 13:03:10 +0530

    Added lifestyle, dependent, incidental, miscellaneous Fbudget miniform and calculate the total

---
+++ src/components/generic/file/budget/FMiscExpensesFBudgetMiniFormMDP.ts
@@ -0,0 +1,30 @@
+import { FFormChildMDP } from "@/components/generic/form/FFormMDP";
+import FBudgetMiniFormMDP from "@/components/generic/form/field/FBudgetMiniFormMDP";
+import FCurrencyFieldMDP from "@/components/generic/form/field/FCurrencyFieldMDP";
+
+export default class FMiscExpensesFBudgetMiniFormMDP extends FBudgetMiniFormMDP {
+  childMDP = new FFormChildMDP();
+  taskRoot: any;
+  parent: any;
+  constructor({ taskRoot, parent }: { taskRoot: any; parent: any }) {
+    super({
+      parentMDP: new FFormChildMDP(),
+      dataSelectorKey: "miscellaneousExpenses",
+      disabled: taskRoot.taskDisabled,
+      label: "Miscellaneous Expenses",
+      mandatory: true,
+      boundaryClass: "col-4",
+    });
+    this.taskRoot = taskRoot;
+    this.parent = parent;
+
+    this.addField(
+      new FCurrencyFieldMDP({
+        parentMDP: this.childMDP,
+        dataSelectorKey: "miscellaneousExpense",
+        label: "Miscellaneous Expense",
+        mandatory: true,
+      })
+    )
+  }
+}

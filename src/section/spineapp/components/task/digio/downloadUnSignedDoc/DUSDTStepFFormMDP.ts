b0d876662fcff3868687f12cd0e3e6cd9454e48c
govind-freed <govind.ve@freed.care>
2022-09-06 17:42:52 +0530

    Changed download unsigned doc from generic interface to self

--- src/section/spineapp/components/task/digio/downloadUnSignedDoc/DUSDTStepFFormMDP.ts
+++ src/section/spineapp/components/task/digio/downloadUnSignedDoc/DUSDTStepFFormMDP.ts
@@ -1,13 +1,14 @@
 import FBtnMDP, { BtnType } from "@/components/generic/FBtnMDP";
 import FFormMDP, { FFormChildMDP } from "@/components/generic/form/FFormMDP";
 import FTextFieldMDP from "@/components/generic/form/field/FTextFieldMDP";
-import { GenericTaskIntf } from "@/section/spineapp/util/GenericTaskIntf";
+import SelfTaskIntf from "@/section/spineapp/util/task_intf/SelfTaskIntf";
+
 
 export default class DUSDTStepFFormMDP extends FFormMDP {
     childMDP = new FFormChildMDP();
-    taskRoot: GenericTaskIntf;
+    taskRoot: SelfTaskIntf;
     parent: any;
-    constructor({ taskRoot, parent }: { taskRoot: GenericTaskIntf; parent: any }) {
+    constructor({ taskRoot, parent }: { taskRoot: SelfTaskIntf; parent: any }) {
         super({
             myRefName: "downloadUnSignedDocFormRef",
             disabled: taskRoot.taskDisabled,
@@ -52,16 +53,10 @@ export default class DUSDTStepFFormMDP extends FFormMDP {
             })
         ).addAction(
             new FBtnMDP({
-                label: "Save",
-                onClick: this.validateAndSubmit(),
+                label: "Rescue",
+                onClick: this.rescueTask(),
             })
-        ).addAction(
-            new FBtnMDP({
-                label: "Mark Complete",
-                onClick: this.validateAndMarkComplete(),
-                btnType: BtnType.FILLED
-            })
-        );
+        )
     }
 
 
@@ -69,27 +64,9 @@ export default class DUSDTStepFFormMDP extends FFormMDP {
         return this.parent.getMyRef().$refs[this.myRefName][0];
     }
 
-    validateAndSubmit() {
+    rescueTask() {
         return () => {
-            this.getMyRef().submitForm(this.saveTask());
-        };
-    }
-
-    validateAndMarkComplete() {
-        return () => {
-            this.getMyRef().submitForm(this.saveAndMarkCompleteTask());
-        };
-    }
-
-    saveAndMarkCompleteTask() {
-        return () => {
-            this.taskRoot.saveAndMarkCompleteTask();
-        };
-    }
-
-    saveTask() {
-        return () => {
-            this.taskRoot.saveTask();
-        };
+            this.taskRoot.rescueTask();
+        }
     }
 }

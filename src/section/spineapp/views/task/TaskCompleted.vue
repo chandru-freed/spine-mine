b810249c0dbb14f8cf4f2bc8e0100b3e50dbd88f
umesh-freed <umesh.yadav1@freed.care>
2022-09-14 19:19:48 +0530

    add task completed list

--- src/section/spineapp/views/task/TaskCompleted.vue
+++ src/section/spineapp/views/task/TaskCompleted.vue
@@ -1,35 +1,18 @@
 <template>
-    <!-- TASK TAB -->
-    <v-card class="pa-0 ma-0" color="white" outlined min-height="800px">
-      <task-tab></task-tab>
+  <div class="TaskAssignedToMe">
+    <v-card class="pa-0 ma-0" color="transparent">
+      <task-tab v-model="tab"></task-tab>
+      <!--  COMPLETED TASK -->
       <v-data-table
-        min-height="600px"
-        :headers="headers"
-        :items="taskList"
-        sort-by="taskId"
+        :headers="toBeCompletedTaskheaders"
+        :items="completedTaskTList"
         class="elevation-0"
         :search="search"
-        v-model="selected"
-        :single-select="false"
-        show-select
-        item-key="taskId"
-        :disable-pagination="true"
-        hide-default-footer
+        item-key="completedTaskId"
       >
         <template v-slot:top>
           <v-toolbar flat>
-            <v-col class="col-2">
-              <v-select
-                :disabled="selected.length === 0"
-                :items="['Delete', 'Edit', 'Send']"
-                label="Bulk Actions"
-                single-line
-                hide-details
-                outlined
-                rounded
-                dense
-              ></v-select>
-            </v-col>
+            <v-col class="col-2"> </v-col>
             <v-col class="col-7"></v-col>
             <v-col>
               <v-text-field
@@ -47,80 +30,31 @@
           </v-toolbar>
         </template>
         <template v-slot:item.taskName="{ item }">
-          <v-btn text color="primary" @click="gotoTask(item)" >
+          <v-btn text color="primary" @click="gotoTask(item)">
             {{ item.taskName }}
           </v-btn>
         </template>
         <template v-slot:item.displayId="{ item }">
-          <v-btn text color="secondary" @click="gotoFile(item)">
+          <span class="overline">
             {{ item.displayId }}
-          </v-btn>
-        </template>
-        <template v-slot:item.priority="{ item }">
-          <v-chip label small >
-            {{ item.priority }}
-          </v-chip>
-        </template>
-
-        <template v-slot:item.readyTime="{ item }">
-          <span class="grey--text">
-            {{ item.readyTime | date-time }} ({{ item.readyTime | fromNow }})
           </span>
         </template>
-
-        <template v-slot:item.actions="{ item }">
-          <v-btn outlined small color="secondary" @click="pullTask(item)">PULL</v-btn>
-        </template>
-        <!-- <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="editItem(item)">
-            mdi-pencil
-          </v-icon>
-          <v-icon small class="mr-2" @click="deleteItem(item)">
-            mdi-delete
-          </v-icon>
-          <v-menu
-            offset-y
-            left
-            nudge-bottom="14"
-            min-width="230"
-            content-class="user-profile-menu-content"
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-icon small v-bind="attrs" v-on="on">mdi-dots-vertical</v-icon>
-            </template>
-            <v-list dense>
-              <v-list-item link>
-                <v-icon small class="mr-2">mdi-download</v-icon> Download
-              </v-list-item>
-              <v-list-item link>
-                <v-icon small class="mr-2">mdi-eye</v-icon> Preview
-              </v-list-item>
-              <v-list-item link>
-                <v-icon small class="mr-2">mdi-pencil</v-icon> Edit
-              </v-list-item>
-              <v-list-item link>
-                <v-icon small class="mr-2">mdi-delete</v-icon> Delete
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </template> -->
-        <template v-slot:no-data>
-          No Tasks Available
-        </template>
       </v-data-table>
+      <!--  COMPLETED TASK -->
     </v-card>
-    <!--  TASK TAB -->
+  </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop, Emit, Watch } from "vue-property-decorator";
-// import store, * as Store from '@/../src-gen/store';
-// import * as Data from '@/../src-gen/data';
-// import * as ServerData from '@/../src-gen/server-data';
-// import * as Action from '@/../src-gen/action';
+import store, * as Store from "@/../src-gen/store";
+import * as Data from "@/../src-gen/data";
+import * as ServerData from "@/../src-gen/server-data";
+import * as Action from "@/../src-gen/action";
 import TaskTab from "@/section/spineapp/components/task/TaskTab.vue";
 
-import moment from 'moment';
+import moment from "moment";
+import * as RemoteApiPoint from "@/remote-api-point";
 
 @Component({
   components: {
@@ -128,65 +62,39 @@ import moment from 'moment';
   },
 })
 export default class TaskCompleted extends Vue {
-  public tab = 0;
-
+  completedTaskTList: Data.TaskList.CompletedTaskGrid[] = [];
+  tab = 0;
   selected = [];
   search = "";
-  headers = [
-    {
-      text: "Task",
-      align: "start",
-      sortable: false,
-      value: "taskName",
-    },
-    { text: "File", value: "displayId" },
+  fromDate = "2022-09-14";
+  toDate = "2022-09-7";
+  toBeCompletedTaskheaders = [
+    { text: "Display Id", value: "displayId" },
     { text: "CID", value: "cid" },
-    { text: "Priority", value: "priority" },
-    // { text: "Status", value: "taskStatus" },
-    { text: "Ready On", value: "readyTime" },
-    // { text: "Is Suspended", value: "isSuspended" },
-    // { text: "Created On", value: "createdOn" },
-    { text: "Actions", value: "actions", sortable: false },
+    { text: "Task Name", value: "taskName" },
+    { text: "Started Time", value: "startedTime" },
+    { text: "Completed Time", value: "completedTime" },
   ];
-  taskList = [];
-  
 
   mounted() {
-    this.initialize();
+    this.getCompletedTaskList();
   }
 
-  initialize() {
-    this.taskList = [
-      
-    ];
-  }
-
-  
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  
-
-  getColor(calories: any) {
-    if (calories > 400) return "red";
-    else if (calories > 200) return "orange";
-    else return "green";
-  }
-
-  gotoFile(item: any) {
-    this.$router.push({
-      name: "Root.ClientFile.ClientFileDetails",
-      params: { clientFileNumber: item.cid },
-    });
+  getCompletedTaskList() {
+    Action.TaskList.GetCompletedTaskList.execute2(
+      this.fromDate,
+      this.toDate,
+      (output) => {
+        this.completedTaskTList = output;
+      },
+      (err) => {
+        console.error(err);
+      },
+      RemoteApiPoint.BenchApi
+    );
   }
 
   gotoTask(item: any) {
-    this.$router.push({
-      name: "Root.TaskList.Task.TaskDetails",
-      params: { taskId: item.taskId },
-    });
-  }
-
-  pullTask(item: any) {
     this.$router.push({
       name: "Root.TaskList.Task.TaskDetails",
       params: { taskId: item.taskId },
@@ -196,7 +104,6 @@ export default class TaskCompleted extends Vue {
 </script>
 
 <style>
-
 .v-btn {
   text-transform: unset !important;
 }

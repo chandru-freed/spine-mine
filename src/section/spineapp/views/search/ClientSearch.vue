ecff8b21d231c2549b131909b1fb7495fdd87e4a
govind-freed <govind.ve@freed.care>
2022-09-20 17:56:08 +0530

    Added MyClientFiles

--- src/section/spineapp/views/search/ClientSearch.vue
+++ src/section/spineapp/views/search/ClientSearch.vue
@@ -76,8 +76,8 @@ export default class ClientSearch extends ModelVue implements ClientSearchIntf {
 
   clientGridHeaderList = [
     { text: "Client Name", value: "fullName" },
-    { text: "E-mail", value: "emailId" },
     { text: "Mobile", value: "mobile" },
+    { text: "E-mail", value: "emailId" },
     { text: "City", value: "city" },
     { text: "State", value: "state" },
   ];

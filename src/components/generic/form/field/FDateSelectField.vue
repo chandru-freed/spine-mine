2a59a73e82d4d8002c711c7579395921a20799d7
chandrashekar <chandrashekar@CHANDRASHEKAR-PRO.local>
2022-09-30 12:27:05 +0530

    On Click on calendre icon functionality added

--- src/components/generic/form/field/FDateSelectField.vue
+++ src/components/generic/form/field/FDateSelectField.vue
@@ -1,39 +1,40 @@
 <template>
-<div>
-  <v-menu
-    ref="menu"
-    v-model="menu"
-    :close-on-content-click="false"
-    :return-value.sync="modelValue"
-    transition="scale-transition"
-    offset-y
-    min-width="auto"
-  >
-    <template v-slot:activator="{ on, attrs }">
-      <v-text-field
-        :value="dateInDisplayFormat"
-        append-icon="mdi-calendar"
-        readonly
-        v-bind="{...$props,...attrs}"
-        v-on="on"
-      ></v-text-field>
-    </template>
-    <v-date-picker
-      :min="minDate"
-      :max="maxDate"
-      :value="modelValue"
-      @input="(newValue) => (modelValue = newValue)"
-      no-title
-      scrollable
+  <div>
+    <v-menu
+      ref="menu"
+      v-model="menu"
+      :close-on-content-click="false"
+      :return-value.sync="modelValue"
+      transition="scale-transition"
+      offset-y
+      min-width="auto"
     >
-      <v-spacer></v-spacer>
-      <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
-      <v-btn text color="primary" @click="$refs.menu.save(modelValue)">
-        OK
-      </v-btn>
-    </v-date-picker>
-  </v-menu>
-</div>
+      <template v-slot:activator="{ on, attrs }">
+        <v-text-field
+          :value="dateInDisplayFormat"
+          append-icon="mdi-calendar"
+          readonly
+          v-bind="{ ...$props, ...attrs }"
+          v-on="on"
+          @click:append="menu = !menu"
+        ></v-text-field>
+      </template>
+      <v-date-picker
+        :min="minDate"
+        :max="maxDate"
+        :value="modelValue"
+        @input="(newValue) => (modelValue = newValue)"
+        no-title
+        scrollable
+      >
+        <v-spacer></v-spacer>
+        <v-btn text color="primary" @click="menu = false"> Cancel </v-btn>
+        <v-btn text color="primary" @click="$refs.menu.save(modelValue)">
+          OK
+        </v-btn>
+      </v-date-picker>
+    </v-menu>
+  </div>
 </template>
 
 <script lang="ts">
@@ -61,8 +62,9 @@ export default class FDateSelectField extends VTextField {
   futureDaysDisabled: boolean;
 
   @Prop({
-    default: "DD/MM/YYYY"
-  }) dateDisplayFormat: string;
+    default: "DD/MM/YYYY",
+  })
+  dateDisplayFormat: string;
 
   get minDate() {
     return this.pastDaysDisabled ? moment().format("YYYY-MM-DD") : null;
@@ -76,7 +78,9 @@ export default class FDateSelectField extends VTextField {
   }
 
   get dateInDisplayFormat() {
-    return this.modelValue ? moment(this.modelValue).format(this.dateDisplayFormat) : "";
+    return this.modelValue
+      ? moment(this.modelValue).format(this.dateDisplayFormat)
+      : "";
   }
 
   set modelValue(newModelValue: string) {

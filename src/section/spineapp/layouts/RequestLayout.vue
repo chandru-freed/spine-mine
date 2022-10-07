3b1c8995a837ad2da8fe694b9fb97883b6732d86
umesh-freed <umesh.yadav1@freed.care>
2022-10-07 17:12:40 +0530

    remove the comment code

--- src/section/spineapp/layouts/RequestLayout.vue
+++ src/section/spineapp/layouts/RequestLayout.vue
@@ -7,43 +7,16 @@
       :width="rightFocused ? '100%' : '49%'"
       v-if="!leftFocused"
     >
-      <!-- <div class="d-flex justify-end">
-        <v-btn @click="gotoClientFile" text icon color="lighten-2" class="ma-2">
-          <v-icon size="20">mdi-close</v-icon>
-        </v-btn>
-      </div> -->
-      <!-- <div class="d-flex justify-center"> -->
-        <router-view />
-      <!-- </div> -->
+      <router-view />
     </v-navigation-drawer>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import store, * as Store from "@/../src-gen/store";
-import * as Data from "@/../src-gen/data";
-import * as ServerData from "@/../src-gen/server-data";
-import * as Action from "@/../src-gen/action";
-import FBtn from "@/components/generic/FBtn.vue";
-import FForm from "@/components/generic/form/FForm.vue";
-import ModelVue from "@/components/generic/ModelVue";
-import * as Snackbar from "node-snackbar";
-import Helper from "../util/Helper";
 @Component
 export default class RequestLayout extends Vue {
-  clientFileNumber = this.$route.params.clientFileNumber;
-
-  @Store.Getter.ClientFile.ClientFileSummary.clientFileBasicInfo
-  clientFileBasicInfo: Data.ClientFile.ClientFileBasicInfo;
   leftFocused = false;
   rightFocused = true;
-
-  gotoClientFile() {
-    Helper.Router.gotoFile({
-      router: this.$router,
-      clientFileNumber: this.clientFileNumber,
-    });
-  }
 }
 </script>

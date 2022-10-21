0c6b6a53ac592a7963ae1f0e49bd5d5ad74c1c25
umesh-freed <umesh.yadav1@freed.care>
2022-10-21 13:48:05 +0530

    ClientInfoVerificationTask update FTaskStepperMDP

--- src/section/spineapp/components/task/enrollment/clientInfoVerification/CIVTFStepperMDP.ts
+++ src/section/spineapp/components/task/enrollment/clientInfoVerification/CIVTFStepperMDP.ts
@@ -1,24 +1,29 @@
 import FStepperMDP from "@/components/generic/FStepperMDP";
+import FTaskStepperMDP from "@/components/generic/FTaskStepperMDP";
 import MDP from "@/components/generic/MDP";
 
 import ManualTaskIntf from "@/section/spineapp/util/task_intf/ManualTaskIntf";
 import CIVTFFormMDP from "./CIVTFFormMDP";
 
+export default class CIVTFStepperMDP extends FTaskStepperMDP {
+  taskRoot: ManualTaskIntf;
+  parent: any;
+  constructor({ taskRoot }: { taskRoot: ManualTaskIntf }) {
+    super({
+      myRefName: " clientInfoVerificationStepperRef",
+      actionable: !taskRoot.taskDisabled,
+    });
+    this.taskRoot = taskRoot;
+    this.parent = taskRoot;
 
-export default class CIVTFStepperMDP extends FStepperMDP {
-    taskRoot: ManualTaskIntf;
-    parent: any;
-    constructor({ taskRoot }: { taskRoot: ManualTaskIntf }) {
-      super({ myRefName: " clientInfoVerificationStepperRef" });
-      this.taskRoot = taskRoot;
-      this.parent = taskRoot;
+    this.addStep({
+      name: "Verify",
+      stepContent: new CIVTFFormMDP({ taskRoot: taskRoot, parent: this }),
+    });
+  }
 
-      this.addStep({name: "Verify",stepContent: new CIVTFFormMDP({taskRoot: taskRoot, parent: this})})
-    }
-
-    getMyRef() {
-        console.log(this.parent.$refs);
-        return this.taskRoot.$refs[this.myRefName];
-      }
-    
+  getMyRef() {
+    console.log(this.parent.$refs);
+    return this.taskRoot.$refs[this.myRefName];
+  }
 }

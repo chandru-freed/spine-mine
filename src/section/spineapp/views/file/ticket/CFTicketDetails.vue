5756f87beda74e8761c48bfd3a4abe6787efd9c8
umesh-freed <umesh.yadav1@freed.care>
2022-10-28 17:02:52 +0530

    active ticket and completed and ticket layout uodated

--- src/section/spineapp/views/file/ticket/CFTicketDetails.vue
+++ src/section/spineapp/views/file/ticket/CFTicketDetails.vue
@@ -1,62 +1,199 @@
 <template>
   <div class="CFTicketDetails">
-    <h1>This is the Counter page</h1>
-    <h2>Counter: {{counter}}</h2>
-    <button @click="increment">Increment</button>
-    <button @click="decrement">Decrement</button>
-    <h3> Computed (Double) : {{computedCounter}}</h3>
-    <h3> Watching Old Value: {{oldCounterValue}}</h3>
-    <h3> Watching New Value: {{newCounterValue}}</h3>
-  </div>
+    <!-- CLOSE TICKET -->
+    <v-alert text class="ma-5" color="primary" v-if="closeTicketDialog">
+      <div class="text-center py-3">
+        Are you sure want to close this ticket?
+      </div>
+      <div
+        class="d-flex flex-row align-start flex-wrap justify-space-around pa-2"
+      >
+        <FBtn
+          label="Cancel"
+          :on-click="() => (closeTicketDialog = false)"
+          outlined
+          color="primary"
+          Ò
+        />
+        <FBtn
+          label="Close Ticket"
+          :on-click="() => closeTicket()"
+          outlined
+          color="primary"
+        />
+      </div>
+    </v-alert>
+    <!-- CLOSE TICKET -->
+    <!-- ASSIGN TICKET -->
+    <v-card flat class="mx-5" outlined v-if="reAssignTicketDialog">
+      <v-card-title>Re Assign Ticket<v-spacer /> </v-card-title>
+      <component
+        v-if="!!reAssignTicketFormMetaData"
+        :ref="reAssignTicketFormMetaData.myRefName"
+        :is="reAssignTicketFormMetaData.componentName"
+        v-model="reAssignTicketInput"
+        v-bind="reAssignTicketFormMetaData.props"
+      ></component>
+    </v-card>
+    <!-- ASSIGN TICKET -->
 
+    <v-card flat outlined>
+      <v-card-text>
+        <v-card-title
+          >Ticket Summary<v-spacer />
+          <v-btn
+            class="mx-2"
+            v-if="taskSummary.clientFileNumber"
+            color="primary"
+            @click="gotoFile"
+            text
+            >Go To File</v-btn
+          >
+          <v-btn
+            color="primary"
+            class="mx-2"
+            @click="reAssignClicked()"
+            outlined
+            :disabled="taskCompleted"
+            >Assign</v-btn
+          >
+          <v-btn
+            :disabled="taskCompleted"
+            color="primary"
+            class="mx-2"
+            @click="closeTicketDialog = true"
+            >Close Ticket</v-btn
+          >
+        </v-card-title>
+
+        <component
+          v-if="!!ticketSummaryFormMetaData"
+          :ref="ticketSummaryFormMetaData.myRefName"
+          :is="ticketSummaryFormMetaData.componentName"
+          v-model="taskSummary"
+          v-bind="ticketSummaryFormMetaData.props"
+        ></component>
+      </v-card-text>
+      <ticket-comment />
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts">
-
-import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator';
-// import store, * as Store from '@/../src-gen/store';
-// import * as Data from '@/../src-gen/data';
-// import * as ServerData from '@/../src-gen/server-data';
-// import * as Action from '@/../src-gen/action';
-
-@Component
+import Vue from "vue";
+import { Prop, Component } from "vue-property-decorator";
+import * as Data from "@/../src-gen/data";
+import * as Action from "@/../src-gen/action";
+import store, * as Store from "@/../src-gen/store";
+// import AddCommentFFormMDP from './AddCommentFFormMDP';
+import FForm from "@/components/generic/form/FForm.vue";
+import * as Snackbar from "node-snackbar";
+// import TicketSummaryFFormMDP from "./TicketSummaryFFormMDP";
+import FBtn from "@/components/generic/FBtn.vue";
+// import TicketComment from "./TicketComment.vue";
+import TicketSummaryFFormMDP from "../../ticket/TicketSummaryFFormMDP";
+import ReAssignTicketFFormMDP from "../../ticket/ReAssignTicketFFormMDP";
+import TicketComment from "../../ticket/TicketComment.vue";
+// import ReAssignTicketFFormMDP from "./ReAssignTicketFFormMDP";
+@Component({
+  components: {
+    FForm,
+    FBtn,
+    "ticket-comment": TicketComment,
+  },
+})
 export default class CFTicketDetails extends Vue {
+  @Store.Getter.Ticket.TicketSummary.ticketTaskDetails
+  ticketTaskDetails: Data.Ticket.MyTicketTaskDetails;
 
-  public counter: number = 0 ;
+  @Store.Getter.Ticket.TicketSummary.ticketCommentsList
+  ticketCommentsList: Data.Ticket.MyTicketCommentDetails[];
 
-  public oldCounterValue: number = 0;
-  public newCounterValue: number = 0;
+  reAssignTicketDialog: boolean = false;
+  closeTicketDialog: boolean = false;
+  reAssignTicketInput: Data.Ticket.ReAssignTicketInput =
+    new Data.Ticket.ReAssignTicketInput();
 
-
-  public mounted() {
-
+  get ticketSummaryFormMetaData() {
+    return new TicketSummaryFFormMDP({ root: this }).getMetaData();
   }
 
-  public created() {
-
+  get reAssignTicketFormMetaData() {
+    return new ReAssignTicketFFormMDP({ root: this }).getMetaData();
   }
 
-  @Watch('counter') private onCounterChanged(value: number, oldValue: number) {
-    this.oldCounterValue = oldValue;
-    this.newCounterValue = value;
-
+  get taskId() {
+    return this.$route.params.myTicketId;
   }
 
-  private increment() {
-    this.counter += 1;
+  get taskSummary(): any {
+    return this.ticketTaskDetails.taskInput;
   }
 
-  private decrement() {
-    this.counter -= 1;
+  get taskCompleted() {
+    return this.ticketTaskDetails.taskState === "COMPLETED";
+  }
+  mounted() {
+    this.getMyTicketTaskDetails();
+    Action.Ticket.CloseTicket.interested(
+      this.getMyTicketTaskDetailsWithDelay()
+    );
   }
 
-  private get computedCounter(): number {
-    return this.counter * 2;
+  public destroyed() {
+    Action.Ticket.CloseTicket.notInterested(
+      this.getMyTicketTaskDetailsWithDelay()
+    );
   }
 
+  getMyTicketTaskDetails() {
+    Action.Ticket.GetMyTicketTaskDetails.execute1(this.taskId, (output) => {});
+  }
+
+  getMyTicketTaskDetailsWithDelay() {
+    return () => {
+      setTimeout(() => {
+        this.getMyTicketTaskDetails();
+      }, 1000);
+    };
+  }
+
+  reAssignTicket() {
+    this.reAssignTicketInput.taskId = this.taskId;
+    Action.Ticket.ReassignTicket.execute(this.reAssignTicketInput, (output) => {
+      this.reAssignTicketDialog = false;
+      Snackbar.show({
+        text: "Succesfully reassigned",
+        pos: "bottom-center",
+      });
+    });
+  }
+
+  reAssignClicked() {
+    this.reAssignTicketInput = new Data.Ticket.ReAssignTicketInput();
+    this.reAssignTicketDialog = true;
+  }
+  goBack() {
+    this.$router.back();
+  }
+  cancelReAssignTicket() {
+    this.reAssignTicketDialog = false;
+  }
+  closeTicket() {
+    Action.Ticket.CloseTicket.execute2(this.taskId, undefined, (output) => {
+      Snackbar.show({
+        text: "Succesfully closed the ticket",
+        pos: "bottom-center",
+      });
+      this.closeTicketDialog = false;
+    });
+  }
+
+  gotoFile() {
+    this.$router.push({
+      name: "Root.ClientFile.Workarea",
+      params: { clientFileNumber: this.taskSummary.clientFileNumber },
+    });
+  }
 }
-
 </script>
-
-<style>
-</style>

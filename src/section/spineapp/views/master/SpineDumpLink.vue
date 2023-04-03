087fd34615437273a9617d13c083fb691bd5632e
freedtech <97399450+freedtech@users.noreply.github.com>
2023-04-03 15:50:20 +0530

    Spine Dump (Date Range) card added (#27)
    Co-authored-by: umesh-freed <umesh.yadav1@freed.care>

--- src/section/spineapp/views/master/SpineDumpLink.vue
+++ src/section/spineapp/views/master/SpineDumpLink.vue
@@ -3,16 +3,49 @@
     <v-row no-gutters>
       <v-col md="4">
         <v-card class="ma-10" flat>
+          <v-card-title>Spine Dump</v-card-title>
           <v-list class="py-0">
-            <v-list-item >
-              <v-btn text color="primary" @click="fileInfoDump()"
-                ><v-icon size="60" small class="mr-2">mdi-cloud-download</v-icon>File Info
-                Dump</v-btn
+            <v-list-item>
+              <v-btn
+                text
+                color="primary"
+                @click="downloadDump(spineDumpLinks.fileInfoDump)"
+                ><v-icon size="60" small class="mr-2">mdi-cloud-download</v-icon
+                >File Info Dump</v-btn
               >
             </v-list-item>
             <v-list-item>
-              <v-btn text color="primary" @click="fileCreditorsDump()"
-                >
+              <v-btn
+                text
+                color="primary"
+                @click="downloadDump(spineDumpLinks.fileCreditorsDump)"
+              >
+                <v-icon size="60" small class="mr-2">mdi-cloud-download</v-icon>
+                File Creditors Dump</v-btn
+              >
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col md="4">
+        <v-card class="ma-10" flat>
+          <v-card-title>Spine Dump (Date Range)</v-card-title>
+          <v-list class="py-0">
+            <v-list-item>
+              <v-btn
+                text
+                color="primary"
+                @click="downloadDump(spineDumpDateLinks.fileInfoDump)"
+                ><v-icon size="60" small class="mr-2">mdi-cloud-download</v-icon
+                >File Info Dump</v-btn
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-btn
+                text
+                color="primary"
+                @click="downloadDump(spineDumpDateLinks.fileCreditorsDump)"
+              >
                 <v-icon size="60" small class="mr-2">mdi-cloud-download</v-icon>
                 File Creditors Dump</v-btn
               >
@@ -41,20 +74,32 @@ import FSnackbar from "@/fsnackbar";
 })
 export default class SpineDumpLink extends ModelVue {
   spineDumpLinks: any;
+  spineDumpDateLinks: any;
   mounted() {
     this.getSpineDumpLinks();
+    this.getSpineDumpLinksForDate();
   }
 
-  fileInfoDump() {
-    window.location.assign(this.spineDumpLinks.fileInfoDump);
+  downloadDump(url: any) {
+    window.location.assign(url);
   }
 
-  fileCreditorsDump() {
-    window.location.assign(this.spineDumpLinks.fileCreditorsDump);
-  }
+  // fileInfoDump() {
+  //   window.location.assign(this.spineDumpLinks.fileInfoDump);
+  // }
+
+  // fileCreditorsDump() {
+  //   window.location.assign(this.spineDumpLinks.fileCreditorsDump);
+  // }
   getSpineDumpLinks() {
     Action.Spine.GetSpineDumpLinks.execute((output) => {
       this.spineDumpLinks = output;
+    });
+  }
+
+  getSpineDumpLinksForDate() {
+    Action.Spine.GetSpineDumpLinksForDate.execute((output) => {
+      this.spineDumpDateLinks = output;
     });
   }
 }

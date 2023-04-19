d92b1fdc702405a4a5ee864a819977f14d5aa541
umesh-freed <umesh.yadav1@freed.care>
2023-04-19 14:41:33 +0530

    PrepareAmendment Task - Note in the last Step should be mandatory.

--- src/section/spineapp/components/task/amendment/prepareAmendment/step3/PATFinalStepFFormMDP.ts
+++ src/section/spineapp/components/task/amendment/prepareAmendment/step3/PATFinalStepFFormMDP.ts
@@ -1,48 +1,47 @@
-
 import FBtnMDP, { BtnType } from "@/components/generic/FBtnMDP";
 import FFormMDP, { FFormChildMDP } from "@/components/generic/form/FFormMDP";
 import FTextareaMDP from "@/components/generic/form/field/FTextareaMDP";
 import FTextFieldMDP from "@/components/generic/form/field/FTextFieldMDP";
 import Task from "@/section/spineapp/util/Task";
 
-
-
-
-
 export default class PATFinalStepFFormMDP extends FFormMDP {
-    childMDP = new FFormChildMDP();
-    taskRoot: any;
-    parent: any;
-    constructor({ taskRoot, parent }: { taskRoot: any; parent: any }) {
-        super({
-            myRefName: "prepareAmendmentFinalFormRef",
-            disabled: taskRoot.taskDisabled,
-        });
-        this.taskRoot = taskRoot;
-        this.parent = parent;
-        this.addField(
-            new FTextareaMDP({
-              parentMDP: this.childMDP,
-              dataSelectorKey: "taskOutput.reviewNote",
-              label: "Note",
-              mandatory: true,
-              boundaryClass: "col-12",
-            })
-          )
-          .addAction(
-            new FBtnMDP({
-                label: "Mark Complete",
-                onClick: this.saveAndMarkCompleteTask(),
-                btnType: BtnType.FILLED,
-                condition: Task.isMarkCompleteEnabled(this.taskRoot.taskDetails)
-            })
-        );
-    }
+  childMDP = new FFormChildMDP();
+  taskRoot: any;
+  parent: any;
+  constructor({ taskRoot, parent }: { taskRoot: any; parent: any }) {
+    super({
+      myRefName: "prepareAmendmentFinalFormRef",
+      disabled: taskRoot.taskDisabled,
+    });
+    this.taskRoot = taskRoot;
+    this.parent = parent;
+    this.addField(
+      new FTextareaMDP({
+        parentMDP: this.childMDP,
+        dataSelectorKey: "taskOutput.reviewNote",
+        label: "Note",
+        mandatory: true,
+        boundaryClass: "col-12",
+      })
+    ).addAction(
+      new FBtnMDP({
+        label: "Mark Complete",
+        onClick: this.saveAndMarkCompleteTask(),
+        btnType: BtnType.FILLED,
+        condition: Task.isMarkCompleteEnabled(this.taskRoot.taskDetails),
+      })
+    );
+  }
 
-    saveAndMarkCompleteTask() {
-        console.log(this.taskRoot.taskDetails);
-        return () => {
-          this.taskRoot.saveAndMarkCompleteTask();
-        };
-      }
+  getMyRef() {
+    return this.parent.getMyRef().$refs[this.myRefName][0];
+  }
+
+  saveAndMarkCompleteTask() {
+    return () => {
+      this.getMyRef().submitForm(() => {
+        this.taskRoot.saveAndMarkCompleteTask();
+      });
+    };
+  }
 }

4e4e08ad1c075a2e6c182a319b05907708cfac89
Govind <govindve@Sagars-MacBook-Pro.local>
2023-05-15 20:12:24 +0530

    Budget bug fix

--- src/components/generic/file/budget/FBudget.vue
+++ src/components/generic/file/budget/FBudget.vue
@@ -425,7 +425,6 @@ export default class FBudget extends ModelVue {
   }
 
   get financialHealthCheckPercentage() {
-    console.log(this.totalIncomeAmount,"this.totalIncomeAmount")
     if(!!this.totalIncomeAmount && this.totalIncomeAmount!==0) {
     return (this.financialHealthCheckAmount * 100) / this.totalIncomeAmount;
     } else {
--- src/section/spineapp/components/task/enrollment/enrollClientFile/step3/BudgetException.vue
+++ src/section/spineapp/components/task/enrollment/enrollClientFile/step3/BudgetException.vue
@@ -2,11 +2,11 @@
   <div class="col">
     <v-alert color="warning" outlined>
       <div class="col text-center">
-        <div class="text-center"
-          >Hardship check failed.This is because of the funds available with the
+        <div class="text-center">
+          Hardship check failed.This is because of the funds available with the
           client are either positive, zero or negative(Upto -5%).
-          <div class="text-center">Take exception before continuing.</div></div
-        >
+          <div class="text-center">Take exception before continuing.</div>
+        </div>
         <!-- <v-textarea outlined dense rows="2" label="notes"></v-textarea> -->
         <div class="d-flex justify-center">
           <!-- <f-btn label="Take Exception" outlined primary color="warning" :onClick="() => updateExceptionTaken()"/> -->
@@ -54,12 +54,20 @@ export default class BudgetException extends ModelVue {
     const input = new Data.ClientFile.UpdateExceptionTakenListInput();
     this.addNoteInput.clientFileId = this.clientFileId;
     input.clientFileId = this.clientFileId;
-    input.exceptionTakenList =
-      this.modelValue.taskOutput.exceptionTakenList;
+    input.exceptionTakenList = this.modelValue.taskOutput.exceptionTakenList;
+
+    const noIndex = input.exceptionTakenList.indexOf("NO");
+    if (noIndex !== -1) {
+      input.exceptionTakenList.splice(noIndex, 1);
+    }
+
+    
     input.exceptionTakenList.push(Data.ClientFile.EXCEPTION_TAKEN.HARDSHIP.id);
+    console.log(this.taskRoot.taskFormData.taskOutput.budgetInfo,"Budget info")
+    Store.Mutation.ClientFile.ClientFileSummary.UPDATE_BUDGET_INFO(this.modelValue.taskOutput.budgetInfo)
+
     // input.exceptionTakenList.push(Data.ClientFile.EXCEPTION_TAKEN.)
-    input.exceptionApprovedBy =
-      this.modelValue.taskOutput.exceptionApprovedBy;
+    input.exceptionApprovedBy = this.modelValue.taskOutput.exceptionApprovedBy;
     Action.ClientFile.UpdateExceptionTakenList.execute(input, (output) => {
       Action.FiNote.AddNote.execute(this.addNoteInput, (output) => {
         FSnackbar.success("Successfully taken the exception");
